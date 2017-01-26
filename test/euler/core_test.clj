dac8d210252fe9e4d5ac34e9b2b15074ecc116b9
Josh Ponelat <josh@ponelat.com>
2017-01-26 17:12:32 +0200

    fix euler 4

--- test/euler/core_test.clj
+++ test/euler/core_test.clj
@@ -10,6 +10,6 @@
   (testing "Euler problem #3"
     (is (= (euler-3) 6857)))
   (testing "Euler problem #4"
-    (is (= (euler-4) 906690))))
+    (is (= (euler-4) 906609)))
   (testing "Euler problem #5"
     (is (= (euler-5) 232792560))))
